46e175dd8e1487a23a22536b97675eaad7f75805
Elizabeth Clements <BlueDoge@users.noreply.github.com>
2022-06-08 19:24:06 -0700

    Start building out RemoteShell as a class
    A better implementation of Run should be done...

--- src/RemoteShell.cs
+++ src/RemoteShell.cs
@@ -3,11 +3,15 @@
 
 using System;
 using System.Collections.Generic;
+using System.Security;
+using Renci.SshNet;
 
 namespace BlueDogeTools.panic_at_the_loadbalancer
 {
 	public class RemoteShell
 	{
+		string targetIpAddress;
+		string targetScriptFilepath;
 		private bool bIsHealthy = false;
 
 		public bool IsHealthy()
@@ -17,6 +21,52 @@ namespace BlueDogeTools.panic_at_the_loadbalancer
 
 		public RemoteShell(string ipAddress, string scriptFilepath, bool bAutoRun = false)
 		{
+			targetIpAddress = ipAddress;
+			targetScriptFilepath = scriptFilepath;
+
+			if(bAutoRun)
+			{
+				Run();
+			}
+		}
+
+		private void AskForUsername(out SecureString username)
+		{
+			Console.Write("Username ({0}): ");
+			username = Utilities.ReadSecureLine(false); // don't hide it at all
+		}
+
+		private void AskForPassword(out SecureString password)
+		{
+			Console.Write("Password ({0}): ");
+			password = Utilities.ReadSecureLine();
+		}
+
+		public void Run()
+		{
+			if (IsHealthy()) return;
+
+			SecureString username;
+			SecureString password;
+			AskForUsername(out username);
+			AskForPassword(out password);
+			// definitely need to do this differently but this effectively gets what I'm trying to do... so far
+			using (var c = new SftpClient(targetIpAddress, username.ToString(), password.ToString()))
+			{
+				c.KeepAliveInterval = TimeSpan.FromSeconds(60);
+				c.ConnectionInfo.Timeout = TimeSpan.FromMinutes(180);
+				c.Connect();
+				c.UploadFile(File.OpenRead(targetScriptFilepath), "/tmp/bluedoge.patlb.script.sh");
+				c.Disconnect();
+			} // c.Dispose()
+			using (var c = new SshClient(targetIpAddress, username.ToString(), password.ToString()))
+			{
+				c.Connect();
+				c.RunCommand("cat /tmp/bluedoge.patlb.script.sh | sh > /tmp/bluedoge.patlb.transaction.log");
+				c.Disconnect();
+			} // c.Dispose()
+			username.Dispose();
+			password.Dispose();
 		}
 	}
 }
