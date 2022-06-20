f58e538a36591719c543f31bd6785befafb9fcd5
Elizabeth Clements <BlueDoge@users.noreply.github.com>
2022-06-20 01:24:49 -0700

    Refactored Packer.ps1 and added additional documentation

--- Packer.ps1
+++ Packer.ps1
@@ -6,9 +6,7 @@
 
 $SourceRoot = "./src/"
 $ReleaseBinPath = "bin/Release/net6.0/"
-#$DebugBinPath = "bin/Debug/net6.0/"
 $PackagedPath = "./packaged/"
-$PackagedExecutableName = "patlb"
 $MaximumCompileAttempts = 2
 
 
@@ -17,13 +15,8 @@ $WorkingDir = (Get-Location).Path + "\"
 $SourceRoot = $WorkingDir + $SourceRoot
 $PackagedPath = $WorkingDir + $PackagedPath
 $ReleaseBinPath = ($SourceRoot + "bin/Release/net6.0/")
-#$DebugBinPath = ($SourceRoot + "bin/Debug/net6.0/")
 
-#"SourceRoot = " + $SourceRoot
-#"PackagedPath = " + $PackagedPath
-#"ReleaseBinPath = " + $ReleaseBinPath
-
-function Run-ForcedCommand()
+function Start-RawProcess()
 {
     param([string]$program = (throw "Specify a program")
         ,[string]$argumentString = ""
@@ -46,35 +39,6 @@ function Get-PackageIsCompiled()
     Test-Path -Path $ReleaseBinPath -PathType Container
 }
 
-function Create-PackagedDirectory()
-{
-    if(-not(Test-Path -Path $PackagedPath -PathType Container))
-    {
-        $newitem = New-Item $PackagedPath -ItemType Directory
-    }
-}
-
-function Copy-PackagedBinaries()
-{
-    if(Test-Path -Path $ReleaseBinPath -PathType Container)
-    {
-        try
-        {
-            Copy-Item ($ReleaseBinPath+"*.exe") -Destination ($PackagedPath) -Force -Recurse
-            Copy-Item ($ReleaseBinPath+"*.dll") -Destination ($PackagedPath) -Force -Recurse
-            Copy-Item ($ReleaseBinPath+"*.json") -Destination ($PackagedPath) -Force -Recurse
-        }
-        catch
-        {
-            throw $_.Exception.Message
-        }
-    }
-    else
-    {
-        throw "Packer could not access any bins. Please compile the project first."
-    }
-}
-
 if(-not(Get-PackageIsCompiled))
 {
     "Package is not compiled... attempting build..."
@@ -87,12 +51,12 @@ while(-not(Get-PackageIsCompiled) -and ($Steps -lt $MaximumCompileAttempts))
     # Move out of root, step into src
     Set-Location $SourceRoot
     "Building... Attempt($($Steps + 1) of $($MaximumCompileAttempts))"
-    Run-ForcedCommand -program "C:\Windows\System32\cmd.exe" -argumentString "/c `"dotnet build panic-at-the-loadbalancer.sln -c Release`"" -workingDirectory $SourceRoot -waitForExit
+    Start-RawProcess -program "C:\Windows\System32\cmd.exe" -argumentString "/c `"dotnet build panic-at-the-loadbalancer.sln -c Release`"" -workingDirectory $SourceRoot -waitForExit
     $Steps = $Steps + 1
     # Step back out to root
     Set-Location ".."
 }
-
+# Double check nothing went wrong!
 if(-not(Get-PackageIsCompiled))
 {
     throw "Packer couldn't compile project. Critical error!"
@@ -100,6 +64,28 @@ if(-not(Get-PackageIsCompiled))
 
 # Copy those floppies
 "Attempting to package the binaries."
-Create-PackagedDirectory
-Copy-PackagedBinaries
+# Create Packaged directory
+if(-not(Test-Path -Path $PackagedPath -PathType Container))
+{
+    $newitem = New-Item $PackagedPath -ItemType Directory
+}
+# Attempt to populate the Packaged directory
+if(Test-Path -Path $ReleaseBinPath -PathType Container)
+{
+    try
+    {
+        Copy-Item ($ReleaseBinPath+"*.exe") -Destination ($PackagedPath) -Force -Recurse
+        Copy-Item ($ReleaseBinPath+"*.dll") -Destination ($PackagedPath) -Force -Recurse
+        Copy-Item ($ReleaseBinPath+"*.json") -Destination ($PackagedPath) -Force -Recurse
+    }
+    catch
+    {
+        # Ooops..?!
+        throw $_.Exception.Message
+    }
+}
+else
+{
+    throw "Packer could not access any bins. Please compile the project first."
+}
 "Binaries packaged."
--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# PatLB (Panic at the [elastic] LoadBalancer)
+# PatLB (Panic at the [elastic] LoadBalancer) - WORK IN PROGRESS
+
 The intention of this toolkit is to give a System Operator the ability to do the following:
 - Optionally bring a selected EC2 instance off of an Elastic Load Balancer (ELB) Target Group
 - Optionally, post Target Group removal, execute a house cleaning script.
@@ -9,7 +10,7 @@ The intention of this toolkit is to give a System Operator the ability to do the
 
 ## Building and "Packaging" the Project
 Requirements:
--Only works on Windows.
--Needs .NET 6.0 Framework and Visual Studio 2022 on at least Windows 10
+- Only works on Windows.
+- Needs .NET 6.0 Framework and Visual Studio 2022 on at least Windows 10
 
 If you've met the requirements: execute Packer.ps1 in Windows PowerShell (Optionally ISE).
